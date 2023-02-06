52217ed6068d6085c4b485205275cfcd5fea3ee8
ALEXANDRE GOMES DA CRUZ <alexandregoiania@gmail.com>
2023-02-06 16:01:26 -0300

    First class: Position

--- src/application/ChessProgram.java
+++ src/application/ChessProgram.java
@@ -1,14 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class ChessProgram {
 
 	public static void main(String[] args) { // Projeto Sistema de Jogo de Xadrez - Aula 181
 		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
-		
-		
-		
-		
+				
 		
 		
 	}
